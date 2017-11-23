e1b7833777151bdc73b2513632810d6b55ef5d15
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2017-11-23 18:18:54 +0900

    base

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,14 +10,34 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(posts_params)
-    redirect_to posts_path
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    @post.update(posts_params)
+
+    redirect_to posts_path, notice: "編集しました"
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to posts_path, notice: "削除しました"
+  end
+
   def posts_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'posts' => 'posts#index'
   
-  resources :posts, only: [:index, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy] do
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
